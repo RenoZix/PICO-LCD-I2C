703f5ec5b58492f9a128a44df68cc3925038a67f
RenoZix <61284218+RenoZix@users.noreply.github.com>
2024-10-01 16:36:48 -0600

    Update lcd_i2c.c

--- lcd_i2c.c
+++ lcd_i2c.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
@@ -34,6 +35,10 @@ const int LCD_BLINKON = 0x01;
 const int LCD_CURSORON = 0x02;
 const int LCD_DISPLAYON = 0x04;
 
+// flags for display and cursor shift
+const int LCD_MOVERIGHT = 0x04;
+const int LCD_DISPLAYMOVE = 0x08;
+
 // flags for function set
 const int LCD_5x10DOTS = 0x04;
 const int LCD_2LINE = 0x08;
@@ -48,36 +53,37 @@ const int LCD_ENABLE_BIT = 0x04;
 #define LCD_CHARACTER  1
 #define LCD_COMMAND    0
 
+#define LCD_DELAY_US 600
+
 // Estructura del LCD
 typedef struct LCD_I2C {
     i2c_inst_t *i2c; // Instancia de I2C
     uint8_t addr;    // Dirección I2C del LCD
-    int cols;        // Número de columnas del LCD
-    int rows;        // Número de filas del LCD
 
     // Métodos
     void (*clear)(void);    
     void (*set_cursor)(int, int);  
-    void (*print)(const char *, ...); 
+    void (*printf)(const char *, ...);
+    void (*send_char)(char);
     void (*init)(void);
-    void (*lcd_cursor)(bool, bool);
-    void (*create_char)(uint8_t, uint8_t *);
-    void (*backlight)(bool);
+    void (*cursor)(bool, bool);
+    void (*custom_char)(uint8_t, uint8_t []);
 } LCD_I2C;
 
-LCD_I2C *lcd = NULL; // Puntero global
+LCD_I2C *lcd_global = NULL; // Puntero global
 
-/* Función para escribir un byte en I2C */
+// Función para escribir un byte en I2C 
 void i2c_write_byte(uint8_t val) {
-    i2c_write_blocking(lcd->i2c, lcd->addr, &val, 1, false);
+    i2c_write_blocking(lcd_global->i2c, lcd_global->addr, &val, 1, false);
 }
 
+// Activacion de Enable
 void lcd_toggle_enable(uint8_t val) {
-    sleep_us(1); // Aumentar el tiempo de espera si es necesario
+    sleep_us(LCD_DELAY_US); // Aumentar el tiempo de espera si es necesario
     i2c_write_byte(val | LCD_ENABLE_BIT);
-    sleep_us(1);
+    sleep_us(LCD_DELAY_US);
     i2c_write_byte(val & ~LCD_ENABLE_BIT);
-    sleep_us(1);
+    sleep_us(LCD_DELAY_US);
 }
 
 // Enviar un byte al LCD como dos transferencias nibble
@@ -91,45 +97,37 @@ void lcd_send_byte(uint8_t val, int mode) {
     lcd_toggle_enable(low);
 }
 
+// Limpiar pantalla
 void lcd_clear(void) {
     lcd_send_byte(LCD_CLEARDISPLAY, LCD_COMMAND);
 }
 
+// Mover cursor
 void lcd_set_cursor(int line, int position) {
-    int val = (line == 0) ? 0x80 + position : 0xC0 + position;
-    lcd_send_byte(val, LCD_COMMAND);
+    int val;
+
+    if(line == 0) val = 0x00 + position;
+    else if(line == 1) val = 0x40 + position;
+    else if(line == 2) val = 0x00 + position + 0x14;
+    else if(line == 3) val = 0x40 + position + 0x14;
+
+    lcd_send_byte(LCD_SETDDRAMADDR | val, LCD_COMMAND);
 }
 
+// Enviar caracter
 void lcd_char(char val) {
     lcd_send_byte(val, LCD_CHARACTER);
 }
 
-void lcd_string(const char *s) {
+// Enviar caracteres
+void lcd_string(char *s) {
     while (*s) {
         lcd_char(*s++);
     }
 }
 
-void lcd_backlight(bool on) {
-    if (on) {
-        lcd_send_byte(LCD_BACKLIGHT, LCD_COMMAND);
-    } else {
-        lcd_send_byte(0, LCD_COMMAND);
-    }
-}
-
-//Caracter Personalizado
-void lcd_create_char(uint8_t location, uint8_t *char_map) {
-    if (location < 8) { // Asegúrate de que la ubicación esté en el rango permitido
-        lcd_send_byte(LCD_SETCGRAMADDR | (location << 3), LCD_COMMAND); // Dirige la memoria de CGRAM
-        for (int i = 0; i < 8; i++) { // Envía los 8 bytes del carácter
-            lcd_send_byte(char_map[i], LCD_CHARACTER);
-        }
-    }
-}
-
-// Imprimir usando print
-void lcd_print(const char *format, ...) {
+// Imprimir usando printf
+void lcd_printf(const char *format, ...) {
     char buffer[32];
     va_list args;
     va_start(args, format);
@@ -138,17 +136,31 @@ void lcd_print(const char *format, ...) {
     lcd_string(buffer);
 }
 
-// Activar o desactivar el cursor
-void lcd_cursor(bool visible, bool blink) {
-    if (visible) {
-        if (blink) {
+void lcd_cursor(bool cursor, bool blink){
+    if(cursor == true) {
+        if(blink == true) {
             lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSORON | LCD_BLINKON, LCD_COMMAND);
-        } else {
+        }
+        else {
             lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSORON, LCD_COMMAND);
         }
-    } else {
-        // Si no se quiere el cursor, se mantiene el display encendido sin el cursor
+    }
+    else{
         lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
+    }
+}
+
+// Caracter Personalizado en CGRAM
+void lcd_custom_char(uint8_t position, uint8_t data[]) {
+    if(position >= 0 && position < 8){
+        lcd_send_byte(LCD_SETCGRAMADDR | position * 8, LCD_COMMAND); // Set CGRAM address
+
+        for (int i = 0; i < 8; i++) {
+            lcd_send_byte(data[i], LCD_CHARACTER); // Enviar los datos al CGRAM
+        }
+    }
+    else {
+        return;
     }
 }
 
@@ -159,34 +171,33 @@ void lcd_init(void) {
     lcd_send_byte(0x02, LCD_COMMAND);
 
     // Cambia el modo de función basado en el número de filas
-    lcd_send_byte(LCD_FUNCTIONSET | LCD_2LINE | LCD_5x10DOTS, LCD_COMMAND);
-    lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND); // Mostrar el display sin cursor
-    lcd_send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND);
+    lcd_send_byte(LCD_FUNCTIONSET | LCD_2LINE | LCD_5x10DOTS, LCD_COMMAND); //Fuction set
+    lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND); //Display control
+    lcd_send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND); //Entry Mode
+
     lcd_clear();
 }
 
 // Constructor de la clase LCD_I2C
-LCD_I2C LCD_I2C_new(i2c_inst_t *i2c, uint8_t addr, int cols, int rows) {
+LCD_I2C LCD_I2C_new(i2c_inst_t *i2c, uint8_t addr) {
     LCD_I2C *lcd_instance = malloc(sizeof(LCD_I2C)); // Asignar memoria para la nueva instancia
     if (lcd_instance == NULL) return (LCD_I2C){0}; // Manejo de errores
 
     // Configurar los atributos del LCD
     lcd_instance->i2c = i2c;
     lcd_instance->addr = addr;
-    lcd_instance->cols = cols;
-    lcd_instance->rows = rows;
 
     // Asignar las funciones a los métodos
     lcd_instance->init = lcd_init;
     lcd_instance->set_cursor = lcd_set_cursor;
-    lcd_instance->print = lcd_print;
+    lcd_instance->printf = lcd_printf;
+    lcd_instance->send_char = lcd_char;
     lcd_instance->clear = lcd_clear;
-    lcd_instance->lcd_cursor = lcd_cursor;
-    lcd_instance->create_char = lcd_create_char;
-    lcd_instance->backlight = lcd_backlight;
+    lcd_instance->cursor = lcd_cursor;
+    lcd_instance->custom_char = lcd_custom_char;
 
     // Actualizar el puntero global de LCD
-    lcd = lcd_instance;
+    lcd_global = lcd_instance;
 
     return *lcd_instance; // Devolver la instancia
 }
